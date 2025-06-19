95c841c55c3ae9fbcd14fcd97c1089730c3ba248
jamltg <jamletigio@gmail.com>
2025-06-19 16:15:55 +0800

    show topbar socmed links on mobile

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,7 +1,7 @@
 export default function Topbar() {
   return (
-    <div className="h-12 bg-[#1f5175] text-white px-10">
-      <div className="max-w-[1200px] mx-auto h-full w-full flex flex-col items-center justify-center md:flex-row md:justify-between">
+    <div className="h-14 md:h12 bg-[#1f5175] text-white px-10">
+      <div className="max-w-[1200px] mx-auto h-full w-full flex flex-col gap-2 items-center justify-center md:flex-row md:justify-between">
         {/* Left Section: Time & Email */}
         <div className="flex flex-col items-center gap-1 md:flex-row md:gap-6 md:ml-0">
           <div className="flex items-center gap-2">
@@ -15,7 +15,7 @@ export default function Topbar() {
         </div>
 
         {/* Right Section: Icons */}
-        <div className="hidden md:flex gap-4 items-center md:mr-0">
+        <div className="flex gap-4 items-center md:mr-0">
           <a
             href="http://linkedin.com/in/ian-withington"
             target="_blank"
